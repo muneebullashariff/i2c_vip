a32043b2b3c01c055a38cf67841e3d61bb50ffe4
harikautpala <harika.utpal@gmail.com>
2019-05-24 18:50:58 +0530

    slave_driver.sv, slave_sequencer.sv and slave_monitor.sv are added

---
+++ I2C_tb_files/slave_agt_top/slave_driver.sv
@@ -0,0 +1,72 @@
+// ############################################################################
+//
+// Project : Verification of I2C VIP
+//
+// File_name : slave_driver.sv
+//
+// https://github.com/muneebullashariff/i2c_vip
+//
+// ############################################################################
+
+//-----------------------------------------------------------------------------
+// Class: slave_driver
+// Description of the class :
+// This class drives data to the interface 
+//-----------------------------------------------------------------------------
+
+
+
+
+
+//---------------------------------------------
+// Externally defined tasks and functions
+//---------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------
+// Constructor: new
+// Initializes the slave_driver class object
+//
+// Parameters:
+//  name   - instance name of the slave_driver
+//  parent - parent under which this component is created
+//-----------------------------------------------------------------------------
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Function: build_phase
+// Gets the configuration object
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Function: connect_phase
+// Connects the slave_driver to the required interface
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Task: run_phase
+// Initiates the main Driving task 
+//
+// Parameters:
+//  phase - stores the current phase 
+//----------------------------------------------------------------------
---
+++ I2C_tb_files/slave_agt_top/slave_sequencer.sv
@@ -0,0 +1,36 @@
+// ############################################################################
+//
+// Project : Verification of I2C VIP
+//
+// File_name : slave_sequencer.sv
+//
+// https://github.com/muneebullashariff/i2c_vip
+//
+// ############################################################################
+
+//-----------------------------------------------------------------------------
+// Class: slave_sequencer
+// Description of the class :
+// This class controls the flow of sequence_items between
+// slave_sequence and slave_driver
+//-----------------------------------------------------------------------------
+
+
+
+
+
+//---------------------------------------------
+// Externally defined tasks and functions
+//---------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------
+// Constructor: new
+// Initializes the slave_sequencer class object
+//
+// Parameters:
+//  name   - instance name of the slave_sequencer
+//  parent - parent under which this component is created
+//-----------------------------------------------------------------------------
+
