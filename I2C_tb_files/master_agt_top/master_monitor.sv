d7de3512df2ea73ac1b6cebf80b89590336eadfe
harikautpala <harika.utpal@gmail.com>
2019-05-24 18:47:46 +0530

    master_sequencer.sv and master_monitor.sv are added

---
+++ I2C_tb_files/master_agt_top/master_monitor.sv
@@ -0,0 +1,73 @@
+// ############################################################################
+//
+// Project : Verification of I2C VIP
+//
+// File_name : Master_monitor.sv
+//
+// https://github.com/muneebullashariff/i2c_vip
+//
+// ############################################################################
+
+//-----------------------------------------------------------------------------
+// Class: Master_monitor
+// Description of the class :
+// This class samples the DUT signals through virtual interface
+//-----------------------------------------------------------------------------
+
+
+
+
+//---------------------------------------------
+// Externally defined tasks and functions
+//---------------------------------------------
+
+
+
+
+//-----------------------------------------------------------------------------
+// Constructor: new
+// Initializes the master_monitor class object
+//
+// Parameters:
+//  name   - instance name of the master_monitor
+//  parent - parent under which this component is created
+//-----------------------------------------------------------------------------
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Function: build_phase
+// Gets the configuration object
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Function: connect_phase
+// Connects the virtual interface to the interface handle of the monitor
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
+
+
+
+
+//-----------------------------------------------------------------------------
+// Task: run_phase
+// Collects the data
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
---
+++ I2C_tb_files/master_agt_top/master_sequencer.sv
@@ -0,0 +1,36 @@
+// ############################################################################
+//
+// Project : Verification of I2C VIP
+//
+// File_name : Master_sequencer.sv
+//
+// https://github.com/muneebullashariff/i2c_vip
+//
+// ############################################################################
+
+//-----------------------------------------------------------------------------
+// Class: Master_sequencer
+// Description of the class :
+// This class controls the flow of sequence_items between
+// master_sequence and master_driver
+//-----------------------------------------------------------------------------
+
+
+
+
+
+//---------------------------------------------
+// Externally defined tasks and functions
+//---------------------------------------------
+
+
+
+//-----------------------------------------------------------------------------
+// Constructor: new
+// Initializes the master_sequencer class object
+//
+// Parameters:
+//  name   - instance name of the master_sequencer
+//  parent - parent under which this component is created
+//-----------------------------------------------------------------------------
+
