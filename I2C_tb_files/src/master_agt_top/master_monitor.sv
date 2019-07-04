054308cfd55d9bb12ddf6824f489d240cf47338f
Harish G <48345995+harishg1996@users.noreply.github.com>
2019-07-04 10:28:10 +0530

    Update master_monitor.sv

--- I2C_tb_files/src/master_agt_top/master_monitor.sv
+++ I2C_tb_files/src/master_agt_top/master_monitor.sv
@@ -1,12 +1,23 @@
-// ############################################################################
+//  ############################################################################
 //
-// Project : Verification of I2C VIP
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
 //
-// File_name : Master_monitor.sv
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
-// https://github.com/muneebullashariff/i2c_vip
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
 //
-// ############################################################################
+//  ###########################################################################
 
 //-----------------------------------------------------------------------------
 // Class: Master_monitor
@@ -14,10 +25,12 @@
 // This class samples the DUT signals through virtual interface
 //-----------------------------------------------------------------------------
 
+`ifndef master_monitor
+`define master_monitor
+
 
 class master_monitor extends uvm_monitor;
 `uvm_component_utils(master_monitor)
-
 
 master_agent_config mcfg;
 
@@ -61,7 +74,7 @@ endfunction
 
 
 function void master_monitor::build_phase(uvm_phase phase);
-  if(!uvm_config_db#(master_agent_config)::get(this,"","MASTER_AGT_CONFIG",mcfg))
+  if(!uvm_config_db#(master_agent_config)::get(this,"","MASTER_AGENT_CONFIG",mcfg))
    `uvm_fatal("master_monitor","COULDNT GET")
 endfunction
 
@@ -87,3 +100,4 @@ endfunction
 //  phase - stores the current phase 
 //-----------------------------------------------------------------------------
 
+`endif
