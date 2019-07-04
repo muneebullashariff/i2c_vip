085852f03448de4ce6056d5a4236f561fafa9af2
Harish G <48345995+harishg1996@users.noreply.github.com>
2019-07-04 15:31:29 +0530

    Update virtual_sequencer.sv

--- I2C_tb_files/src/env/virtual_sequencer.sv
+++ I2C_tb_files/src/env/virtual_sequencer.sv
@@ -1,12 +1,23 @@
-// ############################################################################
+//  ###########################################################################
 //
-// Project : Verification of I2C VIP
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
 //
-// File_name : virtual_sequencer.sv
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
-// https://github.com/muneebullashariff/i2c_vip
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
 //
-// ############################################################################
+//  ###########################################################################
 
 //-----------------------------------------------------------------------------
 // Class: virtual_sequencer
@@ -15,6 +26,8 @@
 // master agents and slave agents 
 //-----------------------------------------------------------------------------
 
+`ifndef virtual_sequencer
+`define virtual_sequencer
 
 
 class virtual_sequencer extends uvm_sequencer#(uvm_sequence_item);
@@ -22,7 +35,8 @@ class virtual_sequencer extends uvm_sequencer#(uvm_sequence_item);
 
   master_sequencer mseqr[];
   slave_sequencer sseqr[];
-environment_config ecfg;
+  environment_config ecfg;
+
 //---------------------------------------------
 // Externally defined tasks and functions
 //---------------------------------------------
@@ -63,4 +77,4 @@ if(!uvm_config_db#(environment_config)::get(this,"","ENVIRONMENT_CONFIG",ecfg))
 
 endfunction
 
-
+`endif
