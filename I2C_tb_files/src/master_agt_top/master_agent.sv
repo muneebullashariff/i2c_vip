fe2de60f2e2afcd4e0e142cccadeb2ed50a4d13a
Harish G <48345995+harishg1996@users.noreply.github.com>
2019-07-04 15:13:40 +0530

    Update master_agent.sv

--- I2C_tb_files/src/master_agt_top/master_agent.sv
+++ I2C_tb_files/src/master_agt_top/master_agent.sv
@@ -1,18 +1,32 @@
-// ############################################################################
+//  ############################################################################
 //
-// Project : Verification of I2C VIP
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
 //
-// File_name : master_agent.sv
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
-// https://github.com/muneebullashariff/i2c_vip
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License. 
 //
-// ############################################################################
+//  ###########################################################################
+
 
 //-----------------------------------------------------------------------------
 // Class: master_agent
 // Description of the class :
 // This class acts like a container. 
 //-----------------------------------------------------------------------------
+`ifndef master_agent
+`define master_agent
 
 class master_agent extends uvm_agent;
 `uvm_component_utils(master_agent)
@@ -84,3 +98,5 @@ function void master_agent::connect_phase(uvm_phase phase);
   if(mcfg.is_active==UVM_ACTIVE)
     driver.seq_item_port.connect(sequencer.seq_item_export);
 endfunction
+
+`endif
