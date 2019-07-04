f613376baf34437b3f89224f2dd683b0e609939d
Harish G <48345995+harishg1996@users.noreply.github.com>
2019-07-04 10:32:14 +0530

    Update master_sequencer.sv

--- I2C_tb_files/src/master_agt_top/master_sequencer.sv
+++ I2C_tb_files/src/master_agt_top/master_sequencer.sv
@@ -1,19 +1,33 @@
-// ############################################################################
+//  ###########################################################################
 //
-// Project : Verification of I2C VIP
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
 //
-// File_name : Master_sequencer.sv
+//  http://www.apache.org/licenses/LICENSE-2.0
 //
-// https://github.com/muneebullashariff/i2c_vip
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
 //
-// ############################################################################
+//  ###########################################################################
 
 //-----------------------------------------------------------------------------
-// Class: Master_sequencer
+// Class: master_sequencer
 // Description of the class :
 // This class controls the flow of sequence_items between
 // master_sequence and master_driver
 //-----------------------------------------------------------------------------
+
+`ifndef master_sequencer
+`define master_sequencer
 
 class master_sequencer extends uvm_sequencer#(master_xtn);
 `uvm_component_utils(master_sequencer)
@@ -42,3 +56,5 @@ endclass
 function master_sequencer::new(string name="master_sequencer",uvm_component parent);
 	super.new(name,parent);
 endfunction
+
+`endif
