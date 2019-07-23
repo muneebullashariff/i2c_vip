cbc66a2fd2a7bf48d6a738460ccd4149c1d8bda0
Harish G <48345995+harishg1996@users.noreply.github.com>
2019-07-23 23:24:20 +0530

    Updated with necessary modifications

--- I2C_tb_files/src/master_agt_top/master_xtn.sv
+++ I2C_tb_files/src/master_agt_top/master_xtn.sv
@@ -19,76 +19,190 @@
 //
 //  ###########################################################################
 
-
 //-----------------------------------------------------------------------------
-// Class: master_xtn
+// Class: Master_driver
 // Description of the class :
-// This class contains variables that would ultimately be provided to
-// interface
+// This class drives data to the interface 
 //-----------------------------------------------------------------------------
-`ifndef master_xtn
-`define master_xtn
+`ifndef master_driver
+`define master_driver
 
-typedef enum {ADDRESS_7BIT=7, ADDRESS_10BIT=10} slave_address_e; 
+class master_driver extends uvm_driver#(master_xtn);
+`uvm_component_utils(master_driver)
 
-class master_xtn extends uvm_sequence_item;
-`uvm_object_utils(master_xtn)
+virtual i2c_intf vif;
+master_agent_config mcfg;
 
-rand bit start_bit;
-rand bit [9:0]slave_address;
-rand bit rd_wr;
-bit ack;
-rand bit [7:0]write_data;
-rand bit stop_bit;
-rand slave_address_e sl_addr_mode; 
-
-//--------------------------------------------
+//---------------------------------------------
 // Externally defined tasks and functions
 //---------------------------------------------
 
-extern function new(string name="master_xtn");
-extern function void do_print(uvm_printer printer);
+extern function new(string name="master_driver",uvm_component parent);
+extern function void build_phase(uvm_phase phase);
+extern function void connect_phase(uvm_phase phase);
+extern task run_phase(uvm_phase phase);
+extern task drive_to_dut(master_xtn xtn);
+extern task send_start_bit(master_xtn xtn);
+extern task send_slave_address(master_xtn xtn);
+extern task send_write_data(master_xtn xtn);
+extern task send_stop_bit(master_xtn xtn);
 
-constraint START { start_bit==0; }
-constraint STOP  { stop_bit ==1; }
-constraint W_DATA{ write_data    == 8'b1000_0000;}
-constraint S_ADDR{ 
-                  if (sl_addr_mode == ADDRESS_7BIT)  { slave_address == 7'b100_0000; }
-                  if (sl_addr_mode == ADDRESS_10BIT) { slave_address == 10'b00_0000_0001; } 
-                  }
-constraint  slave_address_mode_c { sl_addr_mode == ADDRESS_7BIT; }
+endclass
 
 
-endclass:master_xtn
 //-----------------------------------------------------------------------------
 // Constructor: new
-// Initializes the master_xtn class object
+// Initializes the master_driver class object
 //
 // Parameters:
-//  name   - instance name of the master_xtn
+//  name   - instance name of the master_driver
+//  parent - parent under which this component is created
 //-----------------------------------------------------------------------------
 
 
-function master_xtn::new(string name="master_xtn");
-	super.new(name);
-endfunction:new
-
-
-//-----------------------------------------------------------------------------
-// Function: do_print
-// prints the transaction items when called 
-//
-// Parameters:
-// phase - stores the current phase 
-//-----------------------------------------------------------------------------
-
-function void master_xtn::do_print(uvm_printer printer);
-        super.do_print(printer);
-printer.print_field("START_BIT",	this.start_bit, 	1,	UVM_BIN);
-printer.print_field("STOP_BIT",		this.stop_bit,	        1,	UVM_BIN);
-printer.print_field("RD_WR",		this.rd_wr,	        1,	UVM_BIN);
-printer.print_field("SLAVE_ADDR",	this.slave_address,	7,	UVM_BIN);
-printer.print_field("WRITE_DATA",	this.write_data,	8,	UVM_BIN);
+function master_driver::new(string name="master_driver",uvm_component parent);
+	super.new(name,parent);
 endfunction
+
+
+//-----------------------------------------------------------------------------
+// Function: build_phase
+// Creates the required ports
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
+
+function void master_driver::build_phase(uvm_phase phase);
+  if(!uvm_config_db#(master_agent_config)::get(this,"","MASTER_AGENT_CONFIG",mcfg))
+   `uvm_fatal("master_driver","COULDNT GET")
+endfunction
+
+
+
+//-----------------------------------------------------------------------------
+// Function: connect_phase
+// Connects the master_driver to the required interface
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
+
+function void master_driver::connect_phase(uvm_phase phase);
+vif=mcfg.vif;
+endfunction
+
+
+
+//-----------------------------------------------------------------------------
+// Task: run_phase
+// Initiates the main Driving task 
+//
+// Parameters:
+//  phase - stores the current phase 
+//-----------------------------------------------------------------------------
+
+task master_driver::run_phase(uvm_phase phase);
+
+  fork 
+    forever
+      begin
+       #(mcfg.clk_period) vif.clk_int=!vif.clk_int;
+      end
+  join_none
+
+  forever
+    begin
+ 	seq_item_port.get_next_item(req);
+	  drive_to_dut(req);
+	seq_item_port.item_done;
+    end
+endtask
+
+
+task master_driver::drive_to_dut(master_xtn xtn);
+    send_start_bit(xtn);
+    send_slave_address(xtn);
+  repeat(mcfg.no_of_data_item)
+   begin
+    send_write_data(xtn);
+   end
+   send_stop_bit(xtn);
+endtask: drive_to_dut
+
+//-----------------------------------------------------------------------------
+// Task: send_start_bit
+// sends the start bit of the transaction
+//
+// Parameters:
+//  xtn - stores the transaction object handle 
+//-----------------------------------------------------------------------------
+
+task master_driver::send_start_bit(master_xtn xtn);
+           @(vif.m_drv_cb_ctrl);
+	    vif.m_drv_cb_ctrl.sda_int<=1'b0;//Pulls the line low to indicate start of a transfer
+endtask
+
+
+//-----------------------------------------------------------------------------
+// Task:send_slave_address
+// sends the slave address of slave for write/read operation 
+// 
+// Parameters:
+//  xtn - stores the transaction object handle 
+//-----------------------------------------------------------------------------
+
+task master_driver::send_slave_address(master_xtn xtn);
+
+   // Driving the MSB bit of the slave address
+  for(int i=xtn.sl_addr_mode-1; i>=0; i--)
+   begin
+    @(vif.m_drv_cb_data);
+    vif.m_drv_cb_data.sda_int<=xtn.slave_address[i];
+   end
+   //master decides either read/write operation
+    @(vif.m_drv_cb_data);
+    vif.m_drv_cb_data.sda_int<=xtn.rd_wr;
+    @(vif.m_drv_cb_data);
+    vif.m_drv_cb_data.sda_int<=1'b1;     //Release sda line so that slave can send ack/nack
+
+    endtask
+
+ 
+//-----------------------------------------------------------------------------
+// Task:send_write_data
+// sends the data that is to be written into the slave
+// 
+// Parameters:
+//  xtn - stores the transaction object handle 
+//-----------------------------------------------------------------------------
+
+task master_driver::send_write_data(master_xtn xtn);
+	for(int i=0;i<8;i++)
+	  begin
+	    @(vif.m_drv_cb_data);
+	     vif.m_drv_cb_data.sda_int<=xtn.write_data[7-i];
+	  end
+    	    @(vif.m_drv_cb_data);
+	    vif.m_drv_cb_data.sda_int<=1'b1;     //Release sda line so that slave can send ack/nack
+
+endtask
+
+
+//-----------------------------------------------------------------------------
+// Task:send_stop_bit
+// sends the stop bit to indicate the end of transfer
+// 
+// Parameters:
+//  xtn - stores the transaction object handle 
+//-----------------------------------------------------------------------------
+
+task master_driver::send_stop_bit(master_xtn xtn);
+           @(vif.m_drv_cb_ctrl);
+	   vif.m_drv_cb_ctrl.sda_int<=1'b1;//Releases the line at posedge indicating end of transfer
+endtask
+
 
 `endif
